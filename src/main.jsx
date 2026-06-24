eb31172830a6396ffb72b0c061a2de1d7bbafb39
ClaFernandes <clarice_fernandes@hotmail.com>
2026-06-24 12:24:17 +0100

    fix router basename for vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
+// Em GitHub Actions usa /agendly, em Vercel e localhost usa /
+const basename = import.meta.env.BASE_URL === "/agendly/" ? "/agendly" : "/";
+
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/agendly">
+  <BrowserRouter basename={basename}>
     <App />
   </BrowserRouter>,
 );
