2fb5ae98cce5a66885b309c0032f9046ee9611f2
ClaFernandes <clarice_fernandes@hotmail.com>
2026-06-24 12:38:56 +0100

    revert to github pages only

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
-// Em GitHub Actions usa /agendly, em Vercel e localhost usa /
-const basename = import.meta.env.BASE_URL === "/agendly/" ? "/agendly" : "/";
-
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={basename}>
+  <BrowserRouter basename="/agendly">
     <App />
   </BrowserRouter>,
 );
